d797825b18f980f2249cb3bd9b1ede5068796a08
vokoshyv <ronsuij@gmail.com>
2015-04-07 12:29:21 -0700

    working on friend addition

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,40 +1,43 @@
 var App = function(username){
   this.username = username;
+  this.people = {};
   this.rooms = {};
-  this.room = null;
-  this.friends = []; //push added friends to here
+  this.roomname = null;
   this.messages = {};
 }
 
 App.prototype.init = function(username){
   this.username = username;
-  // this.rooms['4chan']='4chan';
   this.fetch();
-  //this.updateRooms();
-  setInterval(this.displayMessages.bind(this),1000)
+ setInterval(this.fetch.bind(this),1000)
 }
 
 App.prototype.fetch = function(){
+  var instance = this;
   var messages = this.messages;
   var rooms = this.rooms;
   $.ajax({
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data : {
+      "order" : '-createdAt'
+    },
     success: function (data) {
       for (var i = 0; i < data.results.length; i++){
-        var key = (data.results[i].username + (data.results[i].updatedAt)) || null;
+        var key = (jsesc(data.results[i].username)
+         + (data.results[i].updatedAt)) || null;
         if (!messages[key]){
           var tempObj = {
-            "roomname" : data.results[i].roomname,
-            "text" : data.results[i].text,
-            "username" : data.results[i].username,
-            "updatedAt" : data.results[i].updatedAt,
-            "isDisplayed" : false
+            "roomname" : jsesc(data.results[i].roomname),
+            "text" : jsesc(data.results[i].text),
+            "username" : jsesc(data.results[i].username),
+            "updatedAt" : jsesc(data.results[i].updatedAt)
           }
           messages[key] = tempObj;
         }
       }
+      instance.updateScreen();
     },
     error: function (data) {
       console.error('chatterbox: Failed to retrieve message');
@@ -42,20 +45,27 @@ App.prototype.fetch = function(){
   });
 }
 
-App.prototype.displayMessages = function(room){
+// jsesc(,{'escapeEverything': true});
+
+App.prototype.updateScreen = function(){
+  this.updateRooms();
+  this.displayMessages();
+  this.updatePeople();
+}
+
+App.prototype.displayMessages = function(){
+  $(".message").remove();
   for(var key in this.messages){
-    if(!this.messages[key].isDisplayed && this.messages[key].room === room){
+    if(this.messages[key].roomname === this.roomname){
       $(".messageContainer").append('<a class=\"message\"> From:'+this.messages[key].username+'<br />'
         +'@: '+this.messages[key].updatedAt+'<br />'
         +this.messages[key].text+'</a>');
       //apply bold css property for friend messages
-      this.messages[key].isDisplayed = true;
     }
   }
 }
 
 App.prototype.updateRooms = function(){
-  this.rooms = {};
   for(var key in this.messages){
     if(!this.rooms[this.messages[key].roomname]){
       this.rooms[this.messages[key].roomname]=1;
@@ -68,17 +78,31 @@ App.prototype.updateRooms = function(){
 }
 
 App.prototype.updateRoomSelector = function(){
-  $(".roomname").remove();
+  // $(".roomname").remove();
+  var instance = this;
+  var displayedRooms = {};
+  $(".roomname").each(function(){
+    if (!instance.rooms[$(this).text()]){
+      $(this).remove();
+    }
+    else{
+      displayedRooms[$(this).text()] = true;
+    }
+  })
+
   for(var room in this.rooms){
-    $('#roomSelector').append('<option class=\"roomname\">'
-      +room+'</option>');
+    if (!displayedRooms[room]){
+      $('#roomSelector').append('<option class=\"roomname\">'
+        +room+'</option>');
+    }
   }
+  $("#roomSelector").val(this.roomname);
 }
 
-App.prototype.send = function(message,room){
+App.prototype.send = function(message){
   //construct object to send as message
   var messageObject = {
-    'roomname' : room,
+    'roomname' : this.roomname,
     'text' : message,
     'username' : this.username
   }
@@ -98,6 +122,46 @@ App.prototype.send = function(message,room){
   });
 }
 
-App.prototype.createRoom = function(room){
-
+App.prototype.createRoom = function(newRoom){
+  if (!this.rooms[newRoom]){
+    this.rooms[newRoom] = 1;
+    this.roomname = newRoom;
+    this.updateRooms();
+  }
 }
+
+App.prototype.updatePeople = function(){
+  for (var key in this.messages){
+    if (this.people[this.messages[key].username] === undefined){
+      this.people[this.messages[key].username] = false;
+    }
+  }
+  this.displayPeople();
+}
+
+App.prototype.displayPeople = function(){
+  var instance = this;
+  $(".person").remove();
+  for(var key in this.people){
+    var newperson=$("#peopleContainer").append('<button class=\"person\">' + key + '</button>');
+    if(this.people[key]){
+      newperson.attr('class', 'person friend');
+    }
+  }
+ $(".person").click(function() {
+    instance.makeFriend($(this).text());
+  });
+}
+
+App.prototype.makeFriend = function(friend){
+  console.log(this.people[friend],friend);
+  if(this.people[friend] !== undefined){
+    this.people[friend] = true;
+  }
+}
+
+
+
+
+
+
